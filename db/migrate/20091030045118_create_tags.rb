cf4d75b21d33ee4e8bf27b4228964ccfdccc9696
Yuya Kato <yuyakato@gmail.com>
2009-10-30 13:53:44 +0900

    タグモデルのDB定義を追加

--- db/migrate/20091030045118_create_tags.rb
+++ db/migrate/20091030045118_create_tags.rb
@@ -1,8 +1,11 @@
+
 class CreateTags < ActiveRecord::Migration
   def self.up
     create_table :tags do |t|
-
-      t.timestamps
+      # 作成日時
+      t.datetime :created_at, :null => false
+      # タグ名
+      t.string   :name,       :null => false, :limit => 50
     end
   end
 
