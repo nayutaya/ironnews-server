a2164b977d460f7e029a4c5306f876724b5511df
Yuya Kato <yuyakato@gmail.com>
2009-10-30 14:00:10 +0900

    記事タグモデルのDB定義を追加

--- db/migrate/20091030045423_create_taggings.rb
+++ db/migrate/20091030045423_create_taggings.rb
@@ -1,8 +1,15 @@
+
 class CreateTaggings < ActiveRecord::Migration
   def self.up
     create_table :taggings do |t|
-
-      t.timestamps
+      # 作成日時
+      t.datetime :created_at, :null => false
+      # ユーザID
+      t.integer  :user_id,    :null => false
+      # 記事ID
+      t.integer  :article_id, :null => false
+      # タグID
+      t.integer  :tag_id,     :null => false
     end
   end
 
