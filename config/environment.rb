47055d849344d44ce652c8c149536330c96be0e2
Yuya Kato <yuyakato@gmail.com>
2009-10-30 11:59:17 +0900

    依存関係を追加

--- config/environment.rb
+++ config/environment.rb
@@ -19,6 +19,15 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
+  config.gem "mongrel"
+  config.gem "nayutaya-kagemusha", :lib => "kagemusha", :version => ">= 0.1.0", :source => "http://gemcutter.org"
+  config.gem "nayutaya-wsse", :lib => "wsse", :version => ">= 0.0.1", :source => "http://gemcutter.org"
+  config.gem "mislav-will_paginate", :version => ">= 2.3.11", :lib => "will_paginate", :source => "http://gems.github.com"
+  config.gem "locale", :version => ">= 2.0.4"
+  config.gem "locale_rails", :version => ">= 2.0.4"
+  config.gem "gettext", :version => ">= 2.0.4"
+  config.gem "gettext_activerecord", :version => ">= 2.0.4"
+  config.gem "gettext_rails", :version => ">= 2.0.4"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
