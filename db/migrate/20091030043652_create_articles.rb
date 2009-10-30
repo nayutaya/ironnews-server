3a1004f78754da202f247a6ba53e056b9cffced2
Yuya Kato <yuyakato@gmail.com>
2009-10-30 13:40:30 +0900

    記事モデルのDB定義を追加

--- db/migrate/20091030043652_create_articles.rb
+++ db/migrate/20091030043652_create_articles.rb
@@ -1,8 +1,15 @@
+
 class CreateArticles < ActiveRecord::Migration
   def self.up
     create_table :articles do |t|
-
-      t.timestamps
+      # 作成日時
+      t.datetime :created_at, :null => false
+      # タイトル
+      t.string   :title,      :null => false, :limit => 200
+      # URLホスト部
+      t.string   :host,       :null => false, :limit => 100
+      # URLパス部
+      t.string   :path,       :null => false, :limit => 200
     end
   end
 
