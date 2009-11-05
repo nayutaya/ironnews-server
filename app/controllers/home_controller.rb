d13181e1fe28aa0e056bf398cb9eeb50c1c01b52
Yuya Kato <yuyakato@gmail.com>
2009-11-05 15:14:28 +0900

    記事の追加処理の検証コードを追加

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,35 @@
 
+require "open-uri"
+
 # ホーム
 class HomeController < ApplicationController
   # TODO: テストせよ
   # GET /
   def index
     @articles = Article.all(:order => "articles.created_at DESC, articles.id DESC")
+  end
+
+  # FIXME: 検証コード
+  def add
+    url = (params[:form] || {})[:url]
+
+    unless url.blank?
+      uri = URI.parse(url)
+      article = Article.new(
+        :title => "-",
+        :host  => uri.host + (uri.port != 80 ? uri.port.to_s : ""),
+        :path  => uri.path)
+
+      api_url = "http://ironnews-helper2.appspot.com/hatena/bookmark/get_title?url1=" + CGI.escape(article.url)
+      open(api_url) { |io|
+        json = io.read
+        ret  = ActiveSupport::JSON.decode(json)
+        article.title = ret[1]["title"]
+      }
+
+      article.save!
+    end
+
+    redirect_to(:action => "index")
   end
 end
