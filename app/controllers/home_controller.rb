109026a25e2620c631c3167ed8455a3fb39e4ca8
Yuya Kato <yuyakato@gmail.com>
2009-11-06 01:38:48 +0900

    記事を連続してみるためのビューを追加

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -36,4 +36,22 @@ class HomeController < ApplicationController
   def view
     @article_ids = params[:article_ids].split(/,/).map(&:to_i)
   end
+
+  def get_info
+    @article_ids = params[:article_ids].split(/,/).map(&:to_i)
+    @callback    = params[:callback]
+
+    articles = Article.all(:conditions => {:id => @article_ids})
+
+    ret = articles.inject({}) { |memo, article|
+      memo[article.id] = {
+        "title" => article.title,
+        "url"   => article.url,
+      }
+      memo
+    }
+
+    json = "#{@callback}(#{ret.to_json})"
+    send_data(json, :type => "text/javascript", :disposition => "inline")
+  end
 end
