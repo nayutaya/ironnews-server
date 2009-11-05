ef27f962fe9fac0e75e2e6635e201d45d9067992
Yuya Kato <yuyakato@gmail.com>
2009-11-06 00:06:10 +0900

    sqlite3-rubyへの依存関係を追加

--- config/environment.rb
+++ config/environment.rb
@@ -17,8 +17,8 @@ Rails::Initializer.run do |config|
   # Specify gems that this application depends on and have them installed with rake gems:install
   # config.gem "bj"
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
-  # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
+  config.gem "sqlite3-ruby", :lib => "sqlite3"
   config.gem "mongrel"
   config.gem "nayutaya-kagemusha", :lib => "kagemusha", :version => ">= 0.1.0", :source => "http://gemcutter.org"
   config.gem "nayutaya-wsse", :lib => "wsse", :version => ">= 0.0.1", :source => "http://gemcutter.org"
