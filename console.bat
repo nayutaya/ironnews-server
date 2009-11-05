48b1e7ce38e4363ff5435bd23fe796f2a8553d67
Yuya Kato <yuyakato@gmail.com>
2009-11-06 00:01:01 +0900

    開発用のバッチファイルに、caesar2用の設定を追加

--- console.bat
+++ console.bat
@@ -1,4 +1,24 @@
-@set PATH=E:\home\bin\ruby-1.8.7-p72\bin;%PATH%
-@set PATH=%PATH%;E:\home\bin\gettext-0.14.4\bin
-@set RUBYOPT=-Itest
-@cmd
+@echo off
+
+if "%COMPUTERNAME%" == "MACBETH" goto MACBETH
+if "%COMPUTERNAME%" == "CAESAR2" goto CAESAR2
+goto ELSE
+
+:MACBETH
+set PATH=E:\home\bin\ruby-1.8.7-p72\bin;%PATH%
+set PATH=E:\home\bin\gettext-0.14.4\bin;%PATH%
+goto END
+
+:CAESAR2
+set PATH=D:\bin\ruby-1.8.7-p72\bin;%PATH%
+goto END
+
+:ELSE
+echo Unknown Host
+goto END
+
+:END
+
+set RUBYOPT=-Itest
+
+cmd
