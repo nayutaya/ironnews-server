150e0422f70b2436b25625e3933393ab5702c622
Yuya Kato <yuyakato@gmail.com>
2009-11-09 13:37:21 +0900

    ユーザモデルに基本的な検証を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,10 +11,14 @@
 
 # ユーザ
 class User < ActiveRecord::Base
+  NameMaxLength = 40 # chars
+  NamePattern   = /\A[A-Za-z0-9_]+\z/
+
   # TODO: テストデータを追加
   # TODO: [関連] Taggingモデルとの関連を追加
   # TODO: [関連] OpenIdCredentialモデルとの関連を追加
-  # TODO: [検証] nameが存在すること
-  # TODO: [検証] nameが40文字以下であること
-  # TODO: [検証] nameが半角英数字で構成されること
+
+  validates_presence_of :name
+  validates_length_of :name, :maximum => NameMaxLength, :allow_blank => true
+  validates_format_of :name, :with => NamePattern, :allow_blank => true
 end
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,8 +1,57 @@
+
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  def setup
+    @klass = User
+    @basic = @klass.new(
+      :name => "name")
+  end
+
+  #
+  # 関連
+  #
+
+  # TODO: 関連を実装せよ
+
+  #
+  # 検証
+  #
+
+  test "all fixtures are valid" do
+    assert_equal(true, @klass.all.all?(&:valid?))
+  end
+
+  test "basic is valid" do
+    assert_equal(true, @basic.valid?)
+  end
+
+  test "validates_presence_of :name" do
+    @basic.name = ""
+    assert_equal(false, @basic.valid?)
+  end
+
+  test "validates_length_of :name" do
+    [
+      ["a" *  1, true ],
+      ["a" * 40, true ],
+      ["a" * 41, false],
+    ].each { |value, expected|
+      @basic.name = value
+      assert_equal(expected, @basic.valid?, value.chars.to_a.size)
+    }
+  end
+
+  test "validates_format_of :name" do
+    [
+      [("0".."9").to_a.join, true ],
+      [("a".."z").to_a.join, true ],
+      [("A".."Z").to_a.join, true ],
+      ["abc_",               true ],
+      ["あいうえお",         false],
+    ].each { |value, expected|
+      @basic.name = value
+      assert_equal(expected, @basic.valid?, value)
+    }
   end
 end
