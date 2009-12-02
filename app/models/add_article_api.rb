1e2d786c0be1a3e38cd909c2ebffa5d7b7dab8d1
Yuya Kato <yuyakato@gmail.com>
2009-12-02 18:54:57 +0900

    記事の登録処理を追加

--- app/models/add_article_api.rb
+++ app/models/add_article_api.rb
@@ -1,2 +1,17 @@
-class AddArticleApi < ActiveRecord::Base
+
+# 記事追加API
+class AddArticleApi < ActiveForm
+  def execute
+    article = Article.new
+    article.title = "title"
+    article.host  = "host"
+    article.path  = "path"
+    article.save!
+
+    result = {
+      :success => true,
+    }
+
+    return result
+  end
 end
--- test/unit/add_article_api_test.rb
+++ test/unit/add_article_api_test.rb
@@ -1,8 +1,34 @@
+
 require 'test_helper'
 
 class AddArticleApiTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  def setup
+    @klass = AddArticleApi
+    @basic = @klass.new
+  end
+
+  #
+  # 検証
+  #
+
+  test "basic is valid" do
+    assert_equal(true, @basic.valid?)
+  end
+
+  #
+  # インスタンスメソッド
+  #
+
+  test "execute" do
+    api = @klass.new
+
+    result = nil
+    assert_difference("Article.count", +1) {
+      result = api.execute
+    }
+    expected = {
+      :success => true,
+    }
+    assert_equal(expected, result)
   end
 end
