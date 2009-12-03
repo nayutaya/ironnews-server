5d40091edfd47888d777726d7e9754f7d24c2bf6
Yuya Kato <yuyakato@gmail.com>
2009-12-03 11:28:15 +0900

    記事を追加するアクションを追加

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,2 +1,20 @@
+
+# API
 class ApiController < ApplicationController
+  def index
+    redirect_to(:controller => "home")
+  end
+
+  def add_article
+    params.delete(:commit)
+    params.delete(:authenticity_token)
+    params.delete(:controller)
+    params.delete(:action)
+    
+    api = AddArticleApi.new(params)
+    result = api.execute
+
+    json = result.to_json
+    send_data(json, :type => "text/javascript", :disposition => "inline")
+  end
 end
