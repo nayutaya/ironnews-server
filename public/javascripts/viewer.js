55686c60080a37cf1e2dbbf66d5174f10262daed
Yuya Kato <yuyakato@gmail.com>
2009-12-04 11:41:24 +0900

    記事の切り替え処理を実装

--- public/javascripts/viewer.js
+++ public/javascripts/viewer.js
@@ -13,7 +13,46 @@ var adjustContainer = function() {
   browser.height(container.outerHeight() - controller.outerHeight())
 };
 
+var articleDB = {};
+var current = 0;
+
+var showArticle = function(id) {
+  var url = articleDB[id].url;
+  /*
+  if ( /www\.asahi\.com/.test(url) ) url += "#HeadLine";
+  else if ( /mainichi\.jp/.test(url) ) url += "#MainBody";
+  */
+  $("#browser").attr("src", url);
+};
+
+var loadArticles = function() {
+  $.ajax({
+    type: "GET",
+    url: "/home/get_info",
+    data: {"article_ids": articleIds.join(",")},
+    dataType: "jsonp",
+    success: function(data){
+      articleDB = data;
+      showArticle(articleIds[0]);
+    },
+  });
+};
+
 $(function() {
   $(window).resize(adjustContainer);
   adjustContainer();
+
+  loadArticles();
+
+  $("#next").click(function() {
+    if ( current < articleIds.length - 1 )
+    {
+      current += 1;
+      showArticle(articleIds[current]);
+    }
+    else
+    {
+      alert("最後の記事です");
+    }
+  });
 });
