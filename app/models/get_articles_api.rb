22ff997d56d15fa85c5414e20ad87162c215b220
Yuya Kato <yuyakato@gmail.com>
2009-12-04 18:26:50 +0900

    記事取得APIフォームに検証を追加

--- app/models/get_articles_api.rb
+++ app/models/get_articles_api.rb
@@ -1,5 +1,10 @@
 
 # 記事取得API
 class GetArticlesApi < ApiBase
+  ArticleIdsFormat = /\A\d+(,\d+)*\z/
+
   column :article_ids, :type => :text
+
+  validates_presence_of :article_ids
+  validates_format_of :article_ids, :with => ArticleIdsFormat, :allow_blank => true
 end
