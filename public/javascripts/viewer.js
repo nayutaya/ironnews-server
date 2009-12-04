01386bfc3a178b72ab9bd2c3856405d29e8add48
Yuya Kato <yuyakato@gmail.com>
2009-12-04 11:29:13 +0900

    閲覧画面のレイアウトを、ウィンドウサイズに合わせて調整するように変更

--- public/javascripts/viewer.js
+++ public/javascripts/viewer.js
@@ -1,1 +1,19 @@
 
+var adjustContainer = function() {
+  var container  = $("#container");
+  var controller = $("#controller");
+  var browser    = $("#browser");
+
+  container.width($(window).width());
+  container.height($(window).height());
+
+  controller.width(container.outerWidth());
+
+  browser.width(container.outerWidth());
+  browser.height(container.outerHeight() - controller.outerHeight())
+};
+
+$(function() {
+  $(window).resize(adjustContainer);
+  adjustContainer();
+});
