e29a5806669f9ed4d05a62659a4815ce257cb058
Yuya Kato <yuyakato@gmail.com>
2009-12-04 14:27:40 +0900

    タグ追加APIモデルにカラムを追加

--- app/models/add_tag_api.rb
+++ app/models/add_tag_api.rb
@@ -1,2 +1,14 @@
-class AddTagApi < ActiveRecord::Base
+# == Schema Information
+# Schema version: 20091030050149
+#
+# Table name: active_forms
+#
+#  article_id :integer
+#  tag        :string
+#
+
+# タグ追加API
+class AddTagApi < ActiveForm
+  column :article_id, :type => :integer
+  column :tag,        :type => :string
 end
