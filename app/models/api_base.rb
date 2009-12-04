141b419d8e2aa9b7aff5600b7db8717a5ff49b87
Yuya Kato <yuyakato@gmail.com>
2009-12-04 15:13:56 +0900

    余分なパラメータを削除するメソッドを追加

--- app/models/api_base.rb
+++ app/models/api_base.rb
@@ -1,2 +1,11 @@
+
+# APIベース
 class ApiBase < ActiveRecord::Base
+  def self.suppress_parameter(params)
+    params = params.dup
+    [:controller, :action, :commit, :authenticity_token, :callback].each { |key|
+      params.delete(key)
+    }
+    return params
+  end
 end
--- test/unit/api_base_test.rb
+++ test/unit/api_base_test.rb
@@ -1,8 +1,29 @@
+
 require 'test_helper'
 
 class ApiBaseTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  def setup
+    @klass = ApiBase
+  end
+
+  #
+  # クラスメソッド
+  #
+
+  test "self.suppress_parameter" do
+    params = {
+      :controller         => "a",
+      :action             => "b",
+      :commit             => "c",
+      :authenticity_token => "d",
+      :callback           => "e",
+      :foo                => "f",
+      :bar                => "g",
+    }
+    expected = {
+      :foo => "f",
+      :bar => "g",
+    }
+    assert_equal(expected, @klass.suppress_parameter(params))
   end
 end
