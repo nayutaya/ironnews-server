3cccd0d9cc5dab855b6b160c9bc20ae67f660387
Yuya Kato <yuyakato@gmail.com>
2009-12-05 16:04:16 +0900

    複数タグ追加APIフォームに検証を追加

--- app/models/add_tags_api.rb
+++ app/models/add_tags_api.rb
@@ -3,4 +3,9 @@
 class AddTagsApi < ApiBase
   column :article_id, :type => :integer
   column :tag1,       :type => :string
+
+  validates_presence_of :article_id
+  validates_presence_of :tag1
+  # FIXME: article_idの存在を検証
+  # FIXME: tagの長さを検証
 end
