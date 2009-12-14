afad9bf432a410755b054deeea6a64beeb1c219b
Yuya Kato <yuyakato@gmail.com>
2009-12-14 11:48:25 +0900

    アノテーション情報を更新

--- app/models/derived_tagging.rb
+++ app/models/derived_tagging.rb
@@ -6,9 +6,8 @@
 #  id         :integer       not null, primary key
 #  created_at :datetime      not null
 #  serial     :integer       not null, index_derived_taggings_on_serial
-#  user_id    :integer       not null, index_derived_taggings_on_user_id_and_article_id_and_tag_id(unique) index_derived_taggings_on_user_id
-#  article_id :integer       not null, index_derived_taggings_on_user_id_and_article_id_and_tag_id(unique) index_derived_taggings_on_article_id
-#  tag_id     :integer       not null, index_derived_taggings_on_user_id_and_article_id_and_tag_id(unique) index_derived_taggings_on_tag_id
+#  article_id :integer       not null, index_derived_taggings_on_article_id_and_tag_id(unique) index_derived_taggings_on_article_id
+#  tag_id     :integer       not null, index_derived_taggings_on_article_id_and_tag_id(unique) index_derived_taggings_on_tag_id
 #
 
 # 導出タグ付け
