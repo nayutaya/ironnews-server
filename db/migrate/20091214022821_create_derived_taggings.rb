8b219a96a1aa880c99ec63e48c2aa997cb9c1271
Yuya Kato <yuyakato@gmail.com>
2009-12-14 11:38:24 +0900

    導出タグ付けモデルのDB定義を追加

--- db/migrate/20091214022821_create_derived_taggings.rb
+++ db/migrate/20091214022821_create_derived_taggings.rb
@@ -1,9 +1,24 @@
+
 class CreateDerivedTaggings < ActiveRecord::Migration
   def self.up
     create_table :derived_taggings do |t|
-
-      t.timestamps
+      # 作成日時
+      t.datetime :created_at, :null => false
+      # 処理済みID
+      t.integer  :serial,     :null => false
+      # ユーザID
+      t.integer  :user_id,    :null => false
+      # 記事ID
+      t.integer  :article_id, :null => false
+      # タグID
+      t.integer  :tag_id,     :null => false
     end
+
+    add_index :derived_taggings, :serial
+    add_index :derived_taggings, :user_id
+    add_index :derived_taggings, :article_id
+    add_index :derived_taggings, :tag_id
+    add_index :derived_taggings, [:user_id, :article_id, :tag_id], :unique => true
   end
 
   def self.down
