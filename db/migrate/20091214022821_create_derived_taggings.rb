b335a9d6c9bcb2d0a7ed9c039f45d6e99d62dbdb
Yuya Kato <yuyakato@gmail.com>
2009-12-14 11:47:54 +0900

    導出タグ付けモデルからユーザIDを削除

--- db/migrate/20091214022821_create_derived_taggings.rb
+++ db/migrate/20091214022821_create_derived_taggings.rb
@@ -4,10 +4,8 @@ class CreateDerivedTaggings < ActiveRecord::Migration
     create_table :derived_taggings do |t|
       # 作成日時
       t.datetime :created_at, :null => false
-      # 処理済みID
+      # 処理済みタグ付けID
       t.integer  :serial,     :null => false
-      # ユーザID
-      t.integer  :user_id,    :null => false
       # 記事ID
       t.integer  :article_id, :null => false
       # タグID
@@ -15,10 +13,9 @@ class CreateDerivedTaggings < ActiveRecord::Migration
     end
 
     add_index :derived_taggings, :serial
-    add_index :derived_taggings, :user_id
     add_index :derived_taggings, :article_id
     add_index :derived_taggings, :tag_id
-    add_index :derived_taggings, [:user_id, :article_id, :tag_id], :unique => true
+    add_index :derived_taggings, [:article_id, :tag_id], :unique => true
   end
 
   def self.down
