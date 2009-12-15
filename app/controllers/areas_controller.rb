53d2c5dd6f75b3a92c766e09428c5f9098ba6da9
Yuya Kato <yuyakato@gmail.com>
2009-12-15 17:21:01 +0900

    各地域版ページを追加

--- app/controllers/areas_controller.rb
+++ app/controllers/areas_controller.rb
@@ -4,8 +4,40 @@ class AreasController < ApplicationController
     show_articles("沖縄")
   end
 
+  def kyushu
+    show_articles("九州")
+  end
+
+  def shikoku
+    show_articles("四国")
+  end
+
+  def chugoku
+    show_articles("中国")
+  end
+
+  def kinki
+    show_articles("近畿")
+  end
+
+  def chubu
+    show_articles("中部")
+  end
+
+  def kanto
+    show_articles("関東")
+  end
+
+  def tohoku
+    show_articles("東北")
+  end
+
   def hokkaido
     show_articles("北海道")
+  end
+
+  def world
+    show_articles("海外")
   end
 
   private
