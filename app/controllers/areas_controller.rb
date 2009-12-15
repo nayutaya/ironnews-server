0b52c1e95961b626de2983b31da871c2aed75b01
Yuya Kato <yuyakato@gmail.com>
2009-12-15 16:01:29 +0900

    北海道のニュース一覧を追加

--- app/controllers/areas_controller.rb
+++ app/controllers/areas_controller.rb
@@ -1,5 +1,16 @@
+
 class AreasController < ApplicationController
   def hokkaido
-  end
+    @rail     = Tag.get("鉄道")
+    @hokkaido = Tag.get("北海道")
 
+    @articles = CombinedTagging.all(
+      :include    => :article,
+      :conditions => [
+        "(combined_taggings.division_tag_id = ?) AND ((combined_taggings.area_tag1_id = ?) OR (combined_taggings.area_tag2_id = ?))",
+        @rail.id,
+        @hokkaido.id,
+        @hokkaido.id,
+      ]).map(&:article)
+  end
 end
