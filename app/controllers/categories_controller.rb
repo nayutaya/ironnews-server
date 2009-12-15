3c041cf7fa84f48ef7f16dbca04a798be9d51bd6
Yuya Kato <yuyakato@gmail.com>
2009-12-15 16:05:45 +0900

    社会カテゴリのニュース一覧ページを追加

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,16 @@
+
 class CategoriesController < ApplicationController
   def social
-  end
+    @rail     = Tag.get("鉄道")
+    @category = Tag.get("社会")
 
+    @articles = CombinedTagging.all(
+      :include    => :article,
+      :conditions => [
+        "(combined_taggings.division_tag_id = ?) AND ((combined_taggings.category_tag1_id = ?) OR (combined_taggings.category_tag2_id = ?))",
+        @rail.id,
+        @category.id,
+        @category.id,
+      ]).map(&:article)
+  end
 end
