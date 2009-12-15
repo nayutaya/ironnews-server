118c8f533525db5e08c71e0405135aef65d76de2
Yuya Kato <yuyakato@gmail.com>
2009-12-15 17:16:01 +0900

    沖縄の地域別ページを追加

--- app/controllers/areas_controller.rb
+++ app/controllers/areas_controller.rb
@@ -1,9 +1,18 @@
 
 class AreasController < ApplicationController
+  def okinawa
+    show_articles("沖縄")
+  end
+
   def hokkaido
-    @articles = Article.
-      division("鉄道").
-      area("北海道").
-      all
+    show_articles("北海道")
+  end
+
+  private
+
+  def show_articles(area)
+    @area     = area
+    @articles = Article.division("鉄道").area(area).all(:order => "articles.created_at DESC")
+    render(:template => "areas/common")
   end
 end
