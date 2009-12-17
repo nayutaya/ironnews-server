0ee79edef07e106b05f73cb337516136c286be71
Yuya Kato <yuyakato@gmail.com>
2009-12-17 17:47:55 +0900

    区分タグが付けられていない記事一覧を追加

--- app/controllers/untagged_articles_controller.rb
+++ app/controllers/untagged_articles_controller.rb
@@ -1,7 +1,26 @@
 
 class UntaggedArticlesController < ApplicationController
   def division
-    
+    user = User.find_by_id(session[:user_id])
+    unless user
+      flash[:notice] = "ログインが必要です"
+      redirect_to(:controller => "home", :action => "index")
+      return
+    end
+
+    tags   = CombinedTagging.get_division_tags
+    recent = Article.all(:select => "id", :order => "articles.created_at DESC", :limit => 500)
+
+    taggings = user.taggings.all(
+      :select     => "article_id",
+      :conditions => ["taggings.tag_id IN (?)", tags.map(&:id).sort])
+
+    tagged_article_ids   = taggings.map(&:article_id).sort.uniq
+    untagged_article_ids = recent.map(&:id) - tagged_article_ids
+
+    @articles = Article.all(
+      :conditions => ["articles.id IN (?)", untagged_article_ids],
+      :order      => "articles.created_at DESC")
   end
 
   def category
