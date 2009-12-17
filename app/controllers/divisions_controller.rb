e53a83e6f03c573f5530134b8dc269c7301f7e69
Yuya Kato <yuyakato@gmail.com>
2009-12-17 17:11:00 +0900

    区分別記事一覧を作成

--- app/controllers/divisions_controller.rb
+++ app/controllers/divisions_controller.rb
@@ -1,2 +1,18 @@
+
 class DivisionsController < ApplicationController
+  def rail
+    show_articles("鉄道")
+  end
+
+  def nonrail
+    show_articles("非鉄")
+  end
+
+  private
+
+  def show_articles(division)
+    @division = division
+    @articles = Article.division(division).all(:order => "articles.created_at DESC")
+    render(:template => "divisions/common")
+  end
 end
