65064ee6ccfe687d3644169f870862569f9d3f4c
Yuya Kato <yuyakato@gmail.com>
2009-12-18 14:40:38 +0900

    複数タグ削除APIフォームにタグの一覧を取得するメソッドを追加

--- app/models/remove_tags_api.rb
+++ app/models/remove_tags_api.rb
@@ -26,6 +26,21 @@ class RemoveTagsApi < ApiBase
   # FIXME: article_idの存在を検証
   # FIXME: tag1の長さを検証
 
+  def tags
+    result = []
+    result << self.tag1  unless self.tag1.blank?
+    result << self.tag2  unless self.tag2.blank?
+    result << self.tag3  unless self.tag3.blank?
+    result << self.tag4  unless self.tag4.blank?
+    result << self.tag5  unless self.tag5.blank?
+    result << self.tag6  unless self.tag6.blank?
+    result << self.tag7  unless self.tag7.blank?
+    result << self.tag8  unless self.tag8.blank?
+    result << self.tag9  unless self.tag9.blank?
+    result << self.tag10 unless self.tag10.blank?
+    return result
+  end
+
   def execute(user_id)
     tag = Tag.get(self.tag1, :create => false)
     if tag
