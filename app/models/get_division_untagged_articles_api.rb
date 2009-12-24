0d63c5638d4d1caa01868c37616599448f7f7c2b
Yuya Kato <yuyakato@gmail.com>
2009-12-24 18:48:25 +0900

    区分タグ未設定記事取得APIフォームに属性を追加

--- app/models/get_division_untagged_articles_api.rb
+++ app/models/get_division_untagged_articles_api.rb
@@ -1,2 +1,6 @@
-class GetDivisionUntaggedArticlesApi < ActiveRecord::Base
+
+# 区分タグ未設定記事取得APIフォーム
+class GetDivisionUntaggedArticlesApi < ApiBase
+  column :page,     :type => :integer
+  column :per_page, :type => :integer
 end
--- test/unit/get_division_untagged_articles_api_test.rb
+++ test/unit/get_division_untagged_articles_api_test.rb
@@ -1,8 +1,24 @@
+
 require 'test_helper'
 
 class GetDivisionUntaggedArticlesApiTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  def setup
+    @klass = GetDivisionUntaggedArticlesApi
+  end
+
+  #
+  # カラム
+  #
+
+  test "columns" do
+    [
+      [:page,     nil, "1", 1],
+      [:per_page, nil, "1", 1],
+    ].each { |name, default, set_value, get_value|
+      form = @klass.new
+      assert_equal(default, form.__send__(name))
+      form.__send__("#{name}=", set_value)
+      assert_equal(get_value, form.__send__(name))
+    }
   end
 end
