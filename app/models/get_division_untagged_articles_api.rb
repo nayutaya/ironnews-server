c533eb2b11b915ff9b779cdd8732529a32cc8174
Yuya Kato <yuyakato@gmail.com>
2009-12-24 18:57:24 +0900

    検証を追加

--- app/models/get_division_untagged_articles_api.rb
+++ app/models/get_division_untagged_articles_api.rb
@@ -3,4 +3,9 @@
 class GetDivisionUntaggedArticlesApi < ApiBase
   column :page,     :type => :integer, :default =>  1
   column :per_page, :type => :integer, :default => 10
+
+  validates_presence_of :page
+  validates_presence_of :per_page
+  validates_numericality_of :page, :greater_than_or_equal_to => 1, :only_integer => true
+  validates_numericality_of :per_page, :greater_than_or_equal_to => 1, :less_than_or_equal_to => 100, :only_integer => true
 end
