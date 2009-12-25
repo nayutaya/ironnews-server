587fe816765af44a1058d5ee742cf95ba37da2e4
Yuya Kato <yuyakato@gmail.com>
2009-12-25 12:05:18 +0900

    ユーザタグ設定済み記事取得APIフォームにカラムを追加

--- app/models/get_user_tagged_articles.rb
+++ app/models/get_user_tagged_articles.rb
@@ -1,2 +1,6 @@
-class GetUserTaggedArticles < ActiveRecord::Base
+
+# ユーザタグ設定済み記事取得API
+class GetUserTaggedArticles < ApiBase
+  column :page,     :type => :integer, :default =>  1
+  column :per_page, :type => :integer, :default => 10
 end
--- test/unit/get_user_tagged_articles_test.rb
+++ test/unit/get_user_tagged_articles_test.rb
@@ -1,8 +1,24 @@
+
 require 'test_helper'
 
 class GetUserTaggedArticlesTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  def setup
+    @klass = GetUserTaggedArticles
+  end
+
+  #
+  # カラム
+  #
+
+  test "columns" do
+    [
+      [:page,      1, "1", 1],
+      [:per_page, 10, "1", 1],
+    ].each { |name, default, set_value, get_value|
+      form = @klass.new
+      assert_equal(default, form.__send__(name))
+      form.__send__("#{name}=", set_value)
+      assert_equal(get_value, form.__send__(name))
+    }
   end
 end
