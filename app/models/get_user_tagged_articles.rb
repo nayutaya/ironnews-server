4e5bf810bcae9d1e665f9440c4fc2b975e0557b4
Yuya Kato <yuyakato@gmail.com>
2009-12-25 12:08:02 +0900

    ユーザタグ設定済み記事取得APIに検証を追加

--- app/models/get_user_tagged_articles.rb
+++ app/models/get_user_tagged_articles.rb
@@ -3,4 +3,9 @@
 class GetUserTaggedArticles < ApiBase
   column :page,     :type => :integer, :default =>  1
   column :per_page, :type => :integer, :default => 10
+
+  validates_presence_of :page
+  validates_presence_of :per_page
+  validates_numericality_of :page, :greater_than_or_equal_to => 1, :only_integer => true, :allow_blank => true
+  validates_numericality_of :per_page, :greater_than_or_equal_to => 1, :less_than_or_equal_to => 100, :only_integer => true, :allow_blank => true
 end
