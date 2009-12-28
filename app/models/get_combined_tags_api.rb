4b4314e00fc59b24b4e5c533203e3047ba721b83
Yuya Kato <yuyakato@gmail.com>
2009-12-28 15:31:03 +0900

    検証を追加

--- app/models/get_combined_tags_api.rb
+++ app/models/get_combined_tags_api.rb
@@ -1,5 +1,10 @@
 
 # 合成タグ取得API
 class GetCombinedTagsApi < ApiBase
+  ArticleIdsFormat = /\A\d+(,\d+)*\z/
+
   column :article_ids, :type => :text
+
+  validates_presence_of :article_ids
+  validates_format_of :article_ids, :with => ArticleIdsFormat, :allow_blank => true
 end
