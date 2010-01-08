523e1fcf1171ba486b4a2b5cdf61b4bd9592dc29
Yuya Kato <yuyakato@gmail.com>
2010-01-08 12:15:17 +0900

    カラムを追加

--- app/models/get_combined_tagged_articles_api.rb
+++ app/models/get_combined_tagged_articles_api.rb
@@ -1,2 +1,9 @@
-class GetCombinedTaggedArticlesApi < ActiveRecord::Base
+
+# 合成タグ設定済み記事取得API
+class GetCombinedTaggedArticlesApi < ApiBase
+  column :division_tag, :type => :text
+  column :category_tag, :type => :text
+  column :area_tag,     :type => :text
+  column :page,         :type => :integer, :default =>  1
+  column :per_page,     :type => :integer, :default => 10
 end
--- test/unit/get_combined_tagged_articles_api_test.rb
+++ test/unit/get_combined_tagged_articles_api_test.rb
@@ -1,8 +1,27 @@
+
 require 'test_helper'
 
 class GetCombinedTaggedArticlesApiTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  def setup
+    @klass = GetCombinedTaggedArticlesApi
+  end
+
+  #
+  # カラム
+  #
+
+  test "columns" do
+    [
+      [:division_tag, nil, "1", "1"],
+      [:category_tag, nil, "1", "1"],
+      [:area_tag,     nil, "1", "1"],
+      [:page,         1,   "1", 1],
+      [:per_page,     10,  "1", 1],
+    ].each { |name, default, set_value, get_value|
+      form = @klass.new
+      assert_equal(default, form.__send__(name))
+      form.__send__("#{name}=", set_value)
+      assert_equal(get_value, form.__send__(name))
+    }
   end
 end
