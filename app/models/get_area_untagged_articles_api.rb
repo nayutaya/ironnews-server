faccd6f0be00c1ac1c195b30742b98950374eaad
Yuya Kato <yuyakato@gmail.com>
2010-01-18 16:23:46 +0900

    カラムを追加

--- app/models/get_area_untagged_articles_api.rb
+++ app/models/get_area_untagged_articles_api.rb
@@ -1,2 +1,6 @@
-class GetAreaUntaggedArticlesApi < ActiveRecord::Base
+
+# 地域タグ未設定記事取得APIフォーム
+class GetAreaUntaggedArticlesApi < ApiBase
+  column :page,     :type => :integer, :default =>  1
+  column :per_page, :type => :integer, :default => 10
 end
