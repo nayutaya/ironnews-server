3fce21c5e24018eab2470eaa7b869c3667a05499
Yuya Kato <yuyakato@gmail.com>
2010-01-20 01:52:11 +0900

    ironnews-utilityを0.3.4に更新

--- public/javascripts/ironnews_utility.js
+++ public/javascripts/ironnews_utility.js
@@ -14,7 +14,7 @@ IronnewsUtility.CleanseTitleTable = {
   "mytown.asahi.com": [[/^asahi\.com:(.+)-マイタウン(.+)$/, "$1 - $2"]],
   "news.kanaloco.jp": [[/^(.+)：ローカルニュース : ニュース : カナロコ -- 神奈川新聞社$/, "$1"]],
   "news24.jp": [[/^(.+) \| 日テレNEWS24$/, "$1"]],
-  "osaka.yomiuri.co.jp": [[/^(.+) : 関西発 : YOMIURI ONLINE（読売新聞）$/, "$1"]],
+  "osaka.yomiuri.co.jp": [[/^(.+) : 関西発 : YOMIURI ONLINE（読売新聞）$/, "$1"],[/^(.+) : (経済　科学)$/, "$1"],[/^(.+) : (経済 ピックアップ|特集|ニュース|人 遊 食)$/, "$1"],[/^(.+) : 地域経済ニュース$/, "$1"]],
   "response.jp": [[/^(.+) \| レスポンス自動車ニュース（Response.jp）$/, "$1"]],
   "sankei.jp.msn.com": [[/^(.+) - MSN産経ニュース$/, "$1"],[/^(.+) \(\d+\/\d+ページ\)$/, "$1"]],
   "slashdot.jp": [[/^(.+) - スラッシュドット・ジャパン$/, "$1"]],
@@ -22,7 +22,7 @@ IronnewsUtility.CleanseTitleTable = {
   "techon.nikkeibp.co.jp": [[/^(.+) - Tech-On！$/, "$1"]],
   "www.47news.jp": [[/^(.+) - 47NEWS（よんななニュース）$/, "$1"],[/^(.+) - 47NEWS \(よんななニュース\)$/, "$1"]],
   "www.afpbb.com": [[/^(.+)　国際ニュース : AFPBB News$/, "$1"]],
-  "www.asahi.com": [[/^asahi\.com（朝日新聞社）：(.+)$/, "$1"],[/^asahi\.com：(.+)$/, "$1"],[/^(.+) - ことばなるほどね！ - 学ぼう$/, "$1"],[/^(.+) - 最新ニュース - 読もう$/, "$1"],[/^asahi\.com （朝日新聞社）：(.+)$/, "$1"]],
+  "www.asahi.com": [[/^asahi\.com（朝日新聞社）：(.+)$/, "$1"],[/^asahi\.com （朝日新聞社）：(.+)$/, "$1"],[/^asahi\.com：(.+)$/, "$1"],[/^(.+) - ことばなるほどね！ - 学ぼう$/, "$1"],[/^(.+) - 最新ニュース - 読もう$/, "$1"],[/^(.+) - (社会|国際|トラベル|ビジネス・経済|政治|サイエンス)$/, "$1"],[/^(.+) - (関西|関西交通・旅ニュース)$/, "$1"]],
   "www.business-i.jp": [[/^(.+) - FujiSankei Business i\.／Bloomberg GLOBAL FINANCE$/, "$1"]],
   "www.chugoku-np.co.jp": [[/^(.+) - 中国新聞$/, "$1"]],
   "www.chunichi.co.jp": [[/^中日新聞:(.+)\(CHUNICHI Web\)$/, "$1"]],
@@ -39,7 +39,7 @@ IronnewsUtility.CleanseTitleTable = {
   "www.nishinippon.co.jp": [[/^(.+) \/ 西日本新聞$/, "$1"]],
   "www.tetsudo.com": [[/^(.+) - 鉄道コム$/, "$1"]],
   "www.tokyo-np.co.jp": [[/^東京新聞:(.+)\(TOKYO Web\)$/, "$1"]],
-  "www.yomiuri.co.jp": [[/^(.+) : YOMIURI ONLINE（読売新聞）$/, "$1"]],
+  "www.yomiuri.co.jp": [[/^(.+) : YOMIURI ONLINE（読売新聞）$/, "$1"],[/^(.+) : (社会|マネー・経済|国際|地域|旅ゅ〜ん！)$/, "$1"],[/^(.+) : 経済ニュース$/, "$1"]],
 //</CleanseTitleTable>
   end: null
 };
