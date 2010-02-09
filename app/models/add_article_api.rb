5c07f831891b8d97df218a21e98c110fa8ec1670
Yuya Kato <yuyakato@gmail.com>
2010-02-09 23:12:56 +0900

    ironnews-helper2 v4を使うように変更

--- app/models/add_article_api.rb
+++ app/models/add_article_api.rb
@@ -17,10 +17,10 @@ class AddArticleApi < ApiBase
 
   # FIXME: 複数のURLのタイトルを一度に取得する
   def self.get_title(url)
-    api_url = "http://v3.latest.ironnews-helper2.appspot.com/hatena-bookmark/get-title?url1=" + CGI.escape(url)
+    api_url = "http://v4.latest.ironnews-helper2.appspot.com/hatena_bookmark/get_title?url=" + CGI.escape(url)
     json = open(api_url) { |io| io.read }
     obj  = ActiveSupport::JSON.decode(json)
-    return obj["1"]["title"]
+    return obj["response"]["title"]
   end
 
   # FIXME: JSONスキーマを追加
